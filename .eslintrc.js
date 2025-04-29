953183549af711bb04618681d4f52ff8dbf877bc
sunggyu-jeong <jskoder0109@gmail.com>
2025-04-29 10:42:23 +0900

    refactor. 프로젝트 패키지 라이브러리 업데이트 및 의존성 오류 수정, eslint 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,57 @@
-import js from '@eslint/js';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+const reactNative = require('eslint-plugin-react-native');
+const prettier = require('eslint-plugin-prettier');
+const globals = require('globals');
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  globals: globals.browser,
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      typescript: {},
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { args: 'none', ignoreRestSiblings: true },
-      ],
-      'import/no-unused-modules': [
-        'warn',
-        { unusedExports: true, missingExports: false },
-      ],
-      'no-unused-expressions': [
-        'error',
-        { allowShortCircuit: false, allowTernary: false, allowTaggedTemplates: false },
-      ],
-      '@typescript-eslint/no-unused-expressions': [
-        'error',
-        { allowShortCircuit: false, allowTernary: false, allowTaggedTemplates: false },
-      ],
-    },
-  }
-);
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react-hooks',
+    'react-refresh',
+    'import',
+    'react-native',
+    'prettier',
+  ],
+  extends: [
+    '@feature-sliced',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { args: 'none', ignoreRestSiblings: true },
+    ],
+    'import/no-unused-modules': ['warn', { unusedExports: true, missingExports: false }],
+    'no-unused-expressions': [
+      'error',
+      { allowShortCircuit: false, allowTernary: false, allowTaggedTemplates: false },
+    ],
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      { allowShortCircuit: false, allowTernary: false, allowTaggedTemplates: false },
+    ],
+  },
+};
