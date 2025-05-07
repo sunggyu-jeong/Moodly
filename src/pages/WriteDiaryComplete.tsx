df01dd566140454516d98ea6beff9c889974ff2e
sunggyu-jeong <jskoder0109@gmail.com>
2025-05-07 16:11:40 +0900

    feat. 일기작성 완료 베타 레이아웃 설정 완료

--- src/pages/WriteDiaryComplete.tsx
+++ src/pages/WriteDiaryComplete.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 
 import { MAIN_ICONS } from '@/shared/assets/images/main';
 import { getScaleSize } from '@/shared/hooks';
 import { navigate } from '@/shared/lib';
+import { gray } from '@/shared/styles/colors';
+import { Body2 } from '@/shared/ui/typography/Body2';
+import { H2 } from '@/shared/ui/typography/H2';
 
 const WriteDiaryComplete = () => {
   useEffect(() => {
@@ -13,24 +16,23 @@ const WriteDiaryComplete = () => {
   }, []);
 
   return (
-    <View className="flex-1 bg-white items-center">
+    <View className="flex-1 bg-common-white items-center justify-center">
       <Image
         source={MAIN_ICONS.avatarComplete}
-        className="w-[144px] h-[144px]"
-        style={{ marginTop: getScaleSize(262) }}
+        style={{ width: getScaleSize(120), height: getScaleSize(120) }}
       />
-      <Text
-        className="font-pretendard font-semibold tracking-[-0.5px]"
-        style={{ marginTop: getScaleSize(42), fontSize: getScaleSize(22) }}
+      <H2
+        weight="semibold"
+        style={{ marginTop: getScaleSize(8), color: gray[600] }}
       >
         일기 작성 완료
-      </Text>
-      <Text
-        className="font-pretendard font-normal text-[#00000080] tracking-[-0.5px]"
-        style={{ marginTop: getScaleSize(13), fontSize: getScaleSize(16) }}
+      </H2>
+      <Body2
+        weight="regular"
+        style={{ marginTop: getScaleSize(8), color: gray[400] }}
       >
         솔직한 마음을 들려줘서 고마워요
-      </Text>
+      </Body2>
     </View>
   );
 };
