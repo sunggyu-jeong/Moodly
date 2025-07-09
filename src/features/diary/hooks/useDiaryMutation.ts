a0cb2240b491b937b358462c38dfb5b859ec7c88
sunggyu-jeong <jskoder0109@gmail.com>
2025-07-09 15:04:30 +0900

    feat. 다이어리 수정 플래그 연동 완료

--- src/features/diary/hooks/useDiaryMutation.ts
+++ src/features/diary/hooks/useDiaryMutation.ts
@@ -6,18 +6,19 @@ import { useCallback } from 'react';
 export function useDiaryMutation(text: string) {
   const todayDiary = useAppSelector(state => state.diarySlice.todayDiary);
   const selectedDiary = useAppSelector(state => state.diarySlice.selectedDiary);
+  const isModifyMode = useAppSelector(state => state.diarySlice.isModifyMode);
   const [create, { data: createRes, isLoading: createLoading }] = useCreateDiaryMutation();
   const [update, { data: updateRes, isLoading: updateLoading }] = useUpdateDiaryMutation();
 
   const save = useCallback(async () => {
     if (isEmpty(text)) return;
     const diary = { ...todayDiary, description: text };
-    if (isEmpty(selectedDiary)) {
-      await create(diary);
+    if (isModifyMode) {
+      await update({ emotionId: selectedDiary?.emotionId ?? -1, updates: diary });
     } else {
-      await update({ emotionId: selectedDiary.emotionId ?? -1, updates: diary });
+      await create(diary);
     }
-  }, [text, create, update, selectedDiary, todayDiary]);
+  }, [text, create, update, selectedDiary, todayDiary, isModifyMode]);
 
   return {
     save,
