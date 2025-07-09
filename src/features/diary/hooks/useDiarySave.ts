7863cfba52e941c0a1a524e92e856a00d9aae422
sunggyu-jeong <jskoder0109@gmail.com>
2025-07-09 11:10:49 +0900

    feat. 일기 저장 로직 쿼리 연동

--- src/features/diary/hooks/useDiarySave.ts
+++ src/features/diary/hooks/useDiarySave.ts
@@ -1,53 +1,51 @@
 // useDiarySave.ts
 
-import { useAppDispatch, useAppSelector, useRealm } from '@shared/hooks';
-import { isNotEmpty, navigate } from '@shared/lib';
+import { useAppDispatch, useAppSelector } from '@shared/hooks';
+import { allValuesNull, isNotEmpty, navigate } from '@shared/lib';
 
-import { setShowToastView } from '@processes/overlay/model/overlay.slice';
-import { addDiaryThunk, modifyDiaryThunk, setSelectedDiary } from '../model/diary.slice';
+import { useCallback, useEffect } from 'react';
+import { useCreateDiaryMutation, useUpdateDiaryMutation } from '../../../shared/api/diary/diaryApi';
+import { setSelectedDiary } from '../model/diary.slice';
 
 export function useDiarySave(text: string) {
   const dispatch = useAppDispatch();
   const todayDiary = useAppSelector(state => state.diarySlice.todayDiary);
   const selectedDiary = useAppSelector(state => state.diarySlice.selectedDiary);
-  const isLogin = useAppSelector(state => state.authSlice.isLogin);
-  const { openRealm, closeRealm } = useRealm();
+  const [createDiary, { data: createDiaryResult, isLoading: isCreateDiaryLoading }] =
+    useCreateDiaryMutation();
+  const [updateDiary, { data: updateDiaryResult, isLoading: isUpdateDiaryLoading }] =
+    useUpdateDiaryMutation();
 
-  const save = async () => {
+  const next = useCallback(() => {
+    const diary = {
+      ...todayDiary,
+      description: text,
+      emotionId: createDiaryResult || updateDiaryResult,
+    };
+    dispatch(setSelectedDiary(diary));
+    navigate('DiaryStack', { screen: 'Complete' });
+  }, [todayDiary, text, createDiaryResult, updateDiaryResult, dispatch]);
+
+  useEffect(() => {
+    if (!allValuesNull(createDiaryResult) || !allValuesNull(updateDiaryResult)) {
+      next();
+    }
+  }, [createDiaryResult, updateDiaryResult, next]);
+
+  const save = useCallback(async () => {
     try {
-      let realm: Realm | undefined;
-      if (!isLogin) {
-        realm = await openRealm();
-      }
-
       if (!isNotEmpty(text)) return;
 
       const diary = { ...todayDiary, description: text };
-      const thunk = isNotEmpty(selectedDiary)
-        ? modifyDiaryThunk({
-            realm,
-            emotionId: selectedDiary.emotionId ?? -1,
-            data: diary,
-            isLogin,
-          })
-        : addDiaryThunk({ realm, data: diary, isLogin });
-
-      const result = await dispatch(thunk);
-      diary.emotionId = result.payload as number;
-      if (!isLogin) {
-        closeRealm();
+      if (isNotEmpty(selectedDiary)) {
+        await updateDiary({ emotionId: selectedDiary.emotionId ?? -1, updates: diary });
+      } else {
+        await createDiary(diary);
       }
-      dispatch(setSelectedDiary(diary));
-      navigate('DiaryStack', { screen: 'Complete' });
     } catch (error) {
       console.error('Diary save failed:', error);
-      dispatch(setShowToastView({ visibility: true, message: '일기 저장 요청이 실패했어요.' }));
-    } finally {
-      if (!isLogin) {
-        closeRealm();
-      }
     }
-  };
+  }, [text, todayDiary, selectedDiary, createDiary, updateDiary]);
 
-  return save;
+  return { save, isCreateDiaryLoading, isUpdateDiaryLoading };
 }
