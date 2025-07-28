7dbfd1cb33a0230d7f7415998c5219f65f2bf98a
sunggyu-jeong <jskoder0109@gmail.com>
2025-07-28 16:55:03 +0900

    feat. 캘린더 연동 완료

--- src/pages/EmotionDiaryListPage.tsx
+++ src/pages/EmotionDiaryListPage.tsx
@@ -1,20 +1,20 @@
-import dayjs from 'dayjs';
-import { useMemo } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-
-import NavigationBar from '@widgets/navigation-bar/ui/NavigationBar.tsx';
-
 import { setSelectedMonth } from '@features/diary/model/diary.slice.ts';
-import { useAppDispatch, useAppSelector } from '@shared/hooks';
-import colors from '@shared/styles/colors.ts';
-
 import EmotionDiaryEmptyMessage from '@features/diary/ui/EmotionDiaryEmptyMessage.tsx';
 import EmotionDiaryMonthSelector from '@features/diary/ui/EmotionDiaryMonthSelector.tsx';
 import DiarySkeleton from '@features/diary/ui/skeleton/DiaryCardSkeleton';
 import { useSelectByMonthQuery } from '@shared/api/diary/diaryApi';
+import { useAppDispatch, useAppSelector } from '@shared/hooks';
 import useDelay from '@shared/hooks/useDelay';
+import colors from '@shared/styles/colors.ts';
+import NavigationBar from '@widgets/navigation-bar/ui/NavigationBar.tsx';
+import dayjs from 'dayjs';
+import { useMemo } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import CalendarBar from '../features/calendar/ui/CalendarBar';
 import EmotionDiaryCardList from '../features/diary/ui/EmotionDiaryCardList';
 import { isEmpty, isNotEmpty } from '../shared/lib';
+import { generateMonthGrid } from '../shared/lib/date.util';
+import WeekdayHeader from '../shared/ui/elements/WeekdayHeader';
 
 const EmotionDiaryListPage = () => {
   const selectedMonth = useAppSelector(state => state.diarySlice.selectedMonth);
@@ -67,9 +67,19 @@ const EmotionDiaryListPage = () => {
         keyExtractor={(item, index) => item.emotionId?.toString() ?? index.toString()}
         contentContainerStyle={[styles.scrollViewContent, isEmpty(data) && styles.emptyContainer]}
         // 데이터 페칭 중일 때 스켈레톤 화면 표출
-        ListHeaderComponent={() => {
-          return showSkeleton || showSkeleton === null ? <DiarySkeleton /> : null;
-        }}
+        // 패칭 완료 시 캘린더 뷰일경우, 캘린더정보 표출
+        ListHeaderComponent={() => (
+          <>
+            {showSkeleton && <DiarySkeleton />}
+            {!showSkeleton && (
+              <View className="relative mb-8">
+                <WeekdayHeader />
+                <CalendarBar monthlyDates={generateMonthGrid(dayjs(selectedMonth).month())} />
+                <View className="w-full left-0 right-0 h-1 bg-gray-200 mt-8" />
+              </View>
+            )}
+          </>
+        )}
         // 빈 상태: 스켈레톤이 끝나고 데이터가 없을 때
         ListEmptyComponent={() =>
           !showSkeleton && (
@@ -89,8 +99,9 @@ const EmotionDiaryListPage = () => {
 const styles = StyleSheet.create({
   emptyContainer: {
     alignItems: 'center',
+    flexGrow: 1,
     justifyContent: 'center',
-    paddingTop: 0,
+    paddingTop: 20,
   },
   scrollViewContent: {
     flexGrow: 1,
