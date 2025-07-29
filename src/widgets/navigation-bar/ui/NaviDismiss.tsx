884bd465cbd11fbe28838eb73a59458c6c2c0d86
sunggyu-jeong <jskoder0109@gmail.com>
2025-07-29 14:10:00 +0900

    feat. 네비게이션 종료 시 편집모드 초기화 로직 삭제

--- src/widgets/navigation-bar/ui/NaviDismiss.tsx
+++ src/widgets/navigation-bar/ui/NaviDismiss.tsx
@@ -1,13 +1,9 @@
-import { setModifyMode } from '@features/diary/model/diary.slice';
 import { COMMON_ICONS } from '@shared/assets/images/common';
-import { useAppDispatch } from '@shared/hooks';
 import { dismissModalToScreen } from '@shared/lib';
 import IconButton from '@shared/ui/elements/IconButton';
 
 const NaviDismiss = () => {
-  const dispatch = useAppDispatch();
   const handleDismiss = () => {
-    dispatch(setModifyMode(false));
     dismissModalToScreen();
   };
 
