ee0cc2cff59c6b825acc6305503f53c3125f06bc
sunggyu-jeong <jskoder0109@gmail.com>
2025-08-22 17:26:50 +0900

    feat. [hot-update] [force] hotupdate trigger

--- src/widgets/setting/ui/SettingWidgetSkeleton.tsx
+++ src/widgets/setting/ui/SettingWidgetSkeleton.tsx
@@ -5,26 +5,24 @@ import SettingWidgetGroup from './SettingWidgetGroup';
 import SettingWidgetHeader from './SettingWidgetHeader';
 import SettingWidgetItem from './SettingWidgetItem';
 
-const SettingWidgetSkeleton = () => {
-  return (
-    <View className="flex-1 bg-gray-100">
-      <View className="flex-1 justify-between pt-[14px]">
-        <View className="flex-1 gap-4">
-          <SettingWidgetHeader />
-          <SettingWidgetGroup>
-            <SettingWidgetItem withToggle />
-          </SettingWidgetGroup>
-          <SettingWidgetGroup>
-            <SettingWidgetItem />
-          </SettingWidgetGroup>
-          <SettingWidgetGroup>
-            <SettingWidgetItem />
-            <SettingWidgetItem />
-          </SettingWidgetGroup>
-        </View>
+const SettingWidgetSkeleton = () => (
+  <View className="flex-1 bg-gray-100">
+    <View className="flex-1 justify-between pt-[14px]">
+      <View className="flex-1 gap-4">
+        <SettingWidgetHeader />
+        <SettingWidgetGroup>
+          <SettingWidgetItem withToggle />
+        </SettingWidgetGroup>
+        <SettingWidgetGroup>
+          <SettingWidgetItem />
+        </SettingWidgetGroup>
+        <SettingWidgetGroup>
+          <SettingWidgetItem />
+          <SettingWidgetItem />
+        </SettingWidgetGroup>
       </View>
     </View>
-  );
-};
+  </View>
+);
 
 export default memo(SettingWidgetSkeleton);
