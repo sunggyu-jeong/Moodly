49fff13c35f3021f528b45cf9435ebba6aca623f
sunggyu-jeong <jskoder0109@gmail.com>
2025-09-07 20:05:37 +0900

    feat. android, ios 배포용 최종버전 업데이트

--- src/shared/ui/elements/DayCell.tsx
+++ src/shared/ui/elements/DayCell.tsx
@@ -1,11 +1,8 @@
-import { selectIsDiaryPagingLoading } from '@features/calendar';
 import { Dayjs } from 'dayjs';
 import { memo } from 'react';
 import { Image, type ImageSourcePropType, TouchableOpacity, View } from 'react-native';
 
-import { useAppSelector, useDelay } from '../../hooks';
 import { Caption } from '../typography';
-import DayCellSkeleton from './Skeleton/DayCellSkeleton';
 
 interface DayCellProps {
   date: Dayjs;
@@ -16,8 +13,6 @@ interface DayCellProps {
 }
 
 const DayCell = ({ date, isSelected, isFuture, iconSource, onPress }: DayCellProps) => {
-  const isDiaryLoading = useAppSelector(selectIsDiaryPagingLoading);
-  const isDelayedLoading = useDelay(isDiaryLoading);
   const renderIcon = () => {
     if (isFuture) {
       return <View className="w-11 h-11 bg-gray-200 rounded-full" />;
@@ -35,33 +30,29 @@ const DayCell = ({ date, isSelected, isFuture, iconSource, onPress }: DayCellPro
   };
   return (
     <View className="items-center bg-common-transparent w-full h-full p-1">
-      {isDelayedLoading ? (
-        <DayCellSkeleton />
-      ) : (
-        <>
-          <TouchableOpacity
-            className="w-10 h-10"
-            onPress={onPress}
-          >
-            {renderIcon()}
-          </TouchableOpacity>
+      <>
+        <TouchableOpacity
+          className="w-10 h-10"
+          onPress={onPress}
+        >
+          {renderIcon()}
+        </TouchableOpacity>
 
-          <View
-            className={
-              isSelected
-                ? 'bg-primary-300 rounded-full w-5/6 items-center mt-2'
-                : 'w-full px-2 items-center mt-2'
-            }
+        <View
+          className={
+            isSelected
+              ? 'bg-primary-300 rounded-full w-5/6 items-center mt-2'
+              : 'w-full px-2 items-center mt-2'
+          }
+        >
+          <Caption
+            weight="regular"
+            className={isSelected ? 'text-common-white' : 'text-gray-500'}
           >
-            <Caption
-              weight="regular"
-              className={isSelected ? 'text-common-white' : 'text-gray-500'}
-            >
-              {date.date() < 10 ? date.format('D') : date.format('DD')}
-            </Caption>
-          </View>
-        </>
-      )}
+            {date.date() < 10 ? date.format('D') : date.format('DD')}
+          </Caption>
+        </View>
+      </>
     </View>
   );
 };
