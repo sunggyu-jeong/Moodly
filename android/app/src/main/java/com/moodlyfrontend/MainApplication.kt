2661568e255312ecde6b643eca3547f1a8ed4927
sunggyu-jeong <jskoder0109@gmail.com>
2025-10-05 22:28:31 +0900

    feat. 안드로이드 설정 추가

--- android/app/src/main/java/com/moodlyfrontend/MainApplication.kt
+++ android/app/src/main/java/com/moodlyfrontend/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.tempproject
+package com.moodlyfrontend
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -9,6 +9,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.moodlyfrontend.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
